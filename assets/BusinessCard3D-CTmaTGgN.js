5d61f0f4efd415951da5b8a71e82e56b5ec4f980
brianjdope <145787578+brianjdope@users.noreply.github.com>
2026-03-26 14:29:47 -0400

    Updates

--- assets/BusinessCard3D-CTmaTGgN.js
+++ assets/BusinessCard3D-CTmaTGgN.js
@@ -1,4 +1,4 @@
-import{r as Pi,j as mr}from"./index-xQXw7lHl.js";/**
+import{r as Pi,j as mr}from"./index-CztcQmzA.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
