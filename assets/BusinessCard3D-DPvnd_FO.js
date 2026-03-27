ec3dc384f5ea776da8679a3c21c367a3451899ba
brianjdope <145787578+brianjdope@users.noreply.github.com>
2026-03-26 23:35:12 -0400

    Updates

--- assets/BusinessCard3D-DPvnd_FO.js
+++ assets/BusinessCard3D-DPvnd_FO.js
@@ -1,4 +1,4 @@
-import{r as Pi,j as mr}from"./index-C7mrJ-Yw.js";/**
+import{r as Pi,j as mr}from"./index-XSm086uN.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
