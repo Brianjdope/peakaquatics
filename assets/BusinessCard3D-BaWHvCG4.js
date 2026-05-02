8af97c17057829caeb43cad8239886360711d529
brianjdope <145787578+brianjdope@users.noreply.github.com>
2026-05-02 10:56:40 -0400

    Updates

--- assets/BusinessCard3D-BaWHvCG4.js
+++ assets/BusinessCard3D-BaWHvCG4.js
@@ -1,4 +1,4 @@
-import{r as Pi,j as mr}from"./index-BmVojcj4.js";/**
+import{r as Pi,j as mr}from"./index-DeH9Kh1H.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
