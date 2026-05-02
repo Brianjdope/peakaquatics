54f42c745e32846b2295ec0449985c1093a9ccb5
brianjdope <145787578+brianjdope@users.noreply.github.com>
2026-05-02 10:50:44 -0400

    Updates

--- assets/BusinessCard3D-CT35Eaw-.js
+++ assets/BusinessCard3D-CT35Eaw-.js
@@ -1,4 +1,4 @@
-import{r as Pi,j as mr}from"./index-CZn7mLeS.js";/**
+import{r as Pi,j as mr}from"./index-BmVojcj4.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
